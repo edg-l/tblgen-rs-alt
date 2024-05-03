824969b03132102cd8cbf20f8678fd80e79aa0da
Yota Toyama <raviqqe@gmail.com>
2024-05-03 18:42:30 +0900

    Fix

--- tools/setup.sh
+++ tools/setup.sh
@@ -5,8 +5,7 @@ set -e
 llvm_version=18
 
 brew update
-brew install llvm@$llvm_version z3
+brew install llvm@$llvm_version
 
-echo TABLEGEN_180_PREFIX=$(brew --prefix)/opt/llvm@$llvm_version >>$GITHUB_ENV
-echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
-echo LIBRARY_PATH=$(brew --prefix)/lib:$LIBRARY_PATH >>$GITHUB_ENV
+echo TABLEGEN_180_PREFIX=$(brew --prefix llvm@$llvm_version) >>$GITHUB_ENV
+echo PATH=$TABLEGEN_180_PREFIX/bin:$PATH >>$GITHUB_ENV
