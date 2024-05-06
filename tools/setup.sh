a74f13a0574df835338239482d13d4ce15ad4ce1
Edgar Luque <git@edgarluque.com>
2024-05-06 13:27:54 +0200

    try

--- tools/setup.sh
+++ tools/setup.sh
@@ -9,3 +9,4 @@ brew install llvm@$llvm_version
 
 echo TABLEGEN_180_PREFIX=$(brew --prefix llvm@$llvm_version) >>$GITHUB_ENV
 echo PATH=$TABLEGEN_180_PREFIX/bin:$PATH >>$GITHUB_ENV
+echo LIBRARY_PATH=/opt/homebrew/lib:$LIBRARY_PATH >>$GITHUB_ENV
