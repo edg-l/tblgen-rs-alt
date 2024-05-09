451ef981e22dfff5261ecf43efa207d085854e12
Edgar <git@edgarluque.com>
2024-05-09 15:36:09 +0200

    try

--- build.rs
+++ build.rs
@@ -75,8 +75,8 @@ fn run() -> Result<(), Box<dyn Error>> {
         println!("cargo:rustc-link-lib={}", name);
     }
 
-    std::env::set_var("CXXFLAGS", llvm_config("--cxxflags")?);
-    std::env::set_var("CFLAGS", llvm_config("--cflags")?);
+    //std::env::set_var("CXXFLAGS", llvm_config("--cxxflags")?);
+    //std::env::set_var("CFLAGS", llvm_config("--cflags")?);
     println!("cargo:rustc-link-search={}", &env::var("OUT_DIR")?);
 
     cc::Build::new()
@@ -89,8 +89,9 @@ fn run() -> Result<(), Box<dyn Error>> {
         .cpp(true)
         .include("cc/include")
         .include(llvm_config("--includedir")?)
-        // .flag("-MJcompile_commands.o.json")
-        .opt_level(3)
+        .flag(&llvm_config("--cxxflags")?)
+        .flag("-Wno-unused-parameter")
+        .std("c++17")
         .compile("CTableGen");
 
     println!("cargo:rustc-link-lib=static=CTableGen");
@@ -99,6 +100,7 @@ fn run() -> Result<(), Box<dyn Error>> {
         .header("wrapper.h")
         .clang_arg(format!("-I{}", "cc/include"))
         .clang_arg(format!("-I{}", llvm_config("--includedir")?))
+        .clang_arg(format!("-L{}", llvm_config("--libdir")?))
         .default_enum_style(bindgen::EnumVariation::ModuleConsts)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
