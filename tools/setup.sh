0412d1b5b42c15e3bb09df91e84077d52bce4921
Yota Toyama <raviqqe@gmail.com>
2024-09-24 22:31:05 +0900

    Fix version number in environment variable

--- tools/setup.sh
+++ tools/setup.sh
@@ -7,7 +7,7 @@ llvm_version=19
 brew update
 brew install llvm@$llvm_version
 
-echo TABLEGEN_180_PREFIX=$(brew --prefix llvm@$llvm_version) >>$GITHUB_ENV
-echo PATH=$TABLEGEN_180_PREFIX/bin:$PATH >>$GITHUB_ENV
+echo TABLEGEN_190_PREFIX=$(brew --prefix llvm@$llvm_version) >>$GITHUB_ENV
+echo PATH=$TABLEGEN_190_PREFIX/bin:$PATH >>$GITHUB_ENV
 echo LIBRARY_PATH=$(brew --prefix)/lib:$LIBRARY_PATH >>$GITHUB_ENV
 echo LD_LIBRARY_PATH=$(brew --prefix)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
