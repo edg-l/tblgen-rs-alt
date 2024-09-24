9f23f26c6681ccc93c5430c2c308955f301eb758
Yota Toyama <raviqqe@gmail.com>
2024-09-24 22:35:08 +0900

    Fix setup.sh

--- tools/setup.sh
+++ tools/setup.sh
@@ -7,7 +7,9 @@ llvm_version=19
 brew update
 brew install llvm@$llvm_version
 
-echo TABLEGEN_190_PREFIX=$(brew --prefix llvm@$llvm_version) >>$GITHUB_ENV
-echo PATH=$TABLEGEN_190_PREFIX/bin:$PATH >>$GITHUB_ENV
+llvm_prefix=$(brew --prefix llvm@$llvm_version)
+
+echo TABLEGEN_190_PREFIX=$llvm_prefix >>$GITHUB_ENV
+echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
 echo LIBRARY_PATH=$(brew --prefix)/lib:$LIBRARY_PATH >>$GITHUB_ENV
 echo LD_LIBRARY_PATH=$(brew --prefix)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
